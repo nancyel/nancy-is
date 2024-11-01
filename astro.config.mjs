642776dc92582416fbb2ced9d313b1a6f79733cd
Nancy Lee <saeryounglee@gmail.com>
2024-11-01 16:56:09 +0900

    add vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,17 @@ import { defineConfig } from "astro/config";
 
 import react from "@astrojs/react";
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astro-react-demo.netlify.app",
+  site: "https://nancy-is.vercel.app",
+  output: "server",
+  prefetch: true,
   integrations: [react()],
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
 });
